e4c87e97d76c9b3b10813ac8bf0f929cedffb729
luissantos-oficina <154244440+luissantos-oficina@users.noreply.github.com>
2024-06-12 09:53:09 +0100

    new

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 console.log("Acabei de ler o script.js");
+var domReady = function(ready) {
+    if (document.readyState != 'loading') return ready();
+    document.addEventListener('DOMContentLoaded', ready);
+    function _ready() {
+        document.removeEventListener('DOMContentLoaded', ready);
+        ready();
+    }
+}
